d2536b028a70709de47db3e5f1b74a9a0879fe67
Paulo Spiguel <paulospill@hotmail.com>
2020-08-01 02:44:35 +0100

    adicionando componentes a home

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React from "react";
 
+import dataInitial from "./data/dados_iniciais.json";
 import Menu from "./components/Menu";
 import Carousel from "./components/Carousel";
 import BannerMain from "./components/BannerMain";
-
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Menu />
+      <BannerMain
+        videoTitle={dataInitial.categorias[0].videos[0].titulo}
+        url={dataInitial.categorias[0].videos[0].url}
+        videoDescription={
+          "O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"
+        }
+      />
+      <Carousel ignoreFirstVideo category={dataInitial.categorias[0]} />
 
+      <Carousel category={dataInitial.categorias[1]} />
+
+      <Carousel category={dataInitial.categorias[2]} />
+
+      <Carousel category={dataInitial.categorias[3]} />
+
+      <Carousel category={dataInitial.categorias[4]} />
+
+      <Carousel category={dataInitial.categorias[5]} />
       <Footer />
     </div>
   );
